ab6107c1ed74e25cc938d064469cd6775fad8bc4
Sergio Carvaho Queiroz <queirozsc@gmail.com>
2019-08-02 15:41:52 -0300

    Remove credenciais
    Remove credenciais de acesso do repositório

--- PowerShell/Requestia/SessionProperties.ps1
+++
@@ -1,8 +0,0 @@
-@{
-    Protocol = [WinSCP.Protocol]::Sftp
-    HostName = "34.197.80.109"
-    UserName = "sftpuser"
-    SshPrivateKeyPath = "C:\Power BI Gateway Files\Requestia\sftpuserPrivate.ppk"
-    PrivateKeyPassphrase = "sftpHO@20190411"
-    SshHostKeyFingerprint = "ssh-rsa 1024 qx844DYjTRTN8gNCw2ykXmlCTecivZpmophsvHyZJCA="
-}
