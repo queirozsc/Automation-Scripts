53d60fbcdba3b76664fc4b53a0ea6e7502b59a09
anderson.santos <gutosoethe@gmail.com>
2019-10-09 17:39:55 -0300

    Upando código para atualizar contatos no 365

---
+++ PowerShell/Office365/Update-Contact.ps1
@@ -0,0 +1,20 @@
+﻿#Faz o update dos Contatos dos Usuário
+#importa CSV
+$Contatos = Import-Csv -Path C:\csv\LicencasOffice365.csv -Delimiter ";"
+
+#Conecta no Office 365
+Connect-MsolService
+#Cria o laço com o Objeto
+$Contatos | ForEach-Object -Process {
+    Write-Host $_
+    #Busca o Usuario no CSV
+    $User = Get-MsolUser -UserPrincipalName $_.UserPrincipalName 
+    #Verifica se o usuarios está Cadastrado
+    if ($User -ne  $null) {
+        Set-MsolUser `
+            -UserPrincipalName $_.UserPrincipalName `
+            -Office $_.Office
+
+    }
+}
+
