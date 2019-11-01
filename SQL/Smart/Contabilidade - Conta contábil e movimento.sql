d6f54f2292dac11dac01ea81d6c206d61688c2bb
Willian Vilson da Cruz <willian.cruz@opty.com.br>
2019-11-01 17:46:08 -0300

    Remoção das contas contábeis na Oftalmoclin

--- SQL/Smart/Contabilidade - Conta contábil e movimento.sql
+++ SQL/Smart/Contabilidade - Conta contábil e movimento.sql
@@ -16,7 +16,7 @@ WHERE mcc_ccr = '20002'
 -- Consulta para verificar todos os movimentos relacionados a conta corrente selecionada
 SELECT mcc_cct_cod, * 
 FROM mcc
-WHERE mcc_cct_cod = 1
+WHERE mcc_cct_cod is not null
 
 -- Update para alterar as movimentações e deixar sem conta corrente
 UPDATE mcc
@@ -29,4 +29,32 @@ WHERE mcc_ccr = '20002'
 -- Delete da conta contábil
 DELETE 
 FROM cct 
-WHERE cct_cod = 1
+WHERE cct_cod = 2
+-----------------------------------------------------------------------------------------------------------------
+UPDATE mcc
+SET mcc_cct_cod = null
+WHERE mcc_cct_cod is not null
+-----------------------------------------------------------------------------------------------------------------
+SELECT mcc_cct_cod, mcc_lote, mcc_seq, mcc_serie, mcc_ccr, mcc_cpg_num FROM mcc WHERE mcc_cct_cod is not null
+-----------------------------------------------------------------------------------------------------------------
+SELECT * FROM cct
+-----------------------------------------------------------------------------------------------------------------
+SELECT top 10 CCR_CCT_COD, * FROM ccr,cct WHERE CCR_CCT_COD = cct.cct_cod
+
+UPDATE ccr
+SET CCR_CCT_COD = null
+where ccr_cod in (88279, 88297) 
+
+select CCR_CCT_COD from ccr where ccr_cod in (88279, 88297)
+-----------------------------------------------------------------------------------------------------------------
+SELECT CFO_CCT_COD, * from cfo WHERE CFO_CCT_COD is not null
+
+UPDATE cfo
+set CFO_CCT_COD = null
+WHERE CFO_CCT_COD is not null
+-----------------------------------------------------------------------------------------------------------------
+SELECT emp_cct_cod, emp_cct_cod_ativo, * FROM emp WHERE emp_cct_cod_ativo is not null
+
+UPDATE emp
+SET emp_cct_cod_ativo = null
+WHERE emp_cct_cod_ativo is not null
