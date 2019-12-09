e6d4e85992f8a0932e48a7971249a5a4d3b00f57
Willian Vilson da Cruz <willian.cruz@opty.com.br>
2019-12-09 09:28:37 -0300

    Updates

--- SQL/Smart/Alteração de CNPJ no compromisso.sql
+++ SQL/Smart/Alteração de CNPJ no compromisso.sql
@@ -1,11 +1,15 @@
 --Alteração de CNPJ na tela de Compromissos a Pagar na Tesouraria
 update cpg 
-set cpg_cic_rg = '12019360000114'
-where cpg_num = 8958
+set cpg_cic_rg = '47960950110067'
+where cpg_num = 4889
 and cpg_serie = 119
 
 
 update cpg 
-set CPG_EMP_COD = '1270'
-where cpg_num = 8958
+set CPG_EMP_COD = '3527'
+where cpg_num = 4889
 and cpg_serie = 119
+
+SELECT * FROM cpg WHERE cpg_num = 4889 AND cpg_serie = 119
+
+SELECT * FROM emp WHERE emp_cgc = '47960950110067'
