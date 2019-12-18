d2773a28174158414eaa711c64fe6b4f30b7fe61
Willian Vilson da Cruz <willian.cruz@opty.com.br>
2019-12-18 09:27:42 -0300

    Alteração Oftalmoclin

--- SQL/Smart/Update Contábil Compromisso.sql
+++ SQL/Smart/Update Contábil Compromisso.sql
@@ -2,13 +2,13 @@
 
 SELECT CPG_CCT_COD_PASSIVO,*
 FROM CPG
-WHERE (CPG.CPG_NUM = 3393) 
+WHERE (CPG.CPG_NUM = 3394) 
     AND (CPG_SERIE = 119)
 
 
 UPDATE CPG
 SET CPG_CCT_COD_PASSIVO = 20040
-WHERE (CPG.CPG_NUM = 3299) 
+WHERE (CPG.CPG_NUM = 3395) 
     AND (CPG_SERIE = 119)
 
 -------------------------------------------------------------------------
@@ -25,7 +25,7 @@ WHERE  ((MCC.MCC_CPG_SERIE = 119)
 UPDATE MCC 
 SET MCC_CCT_COD = 12240
 WHERE  ((MCC.MCC_CPG_SERIE = 119)
-        AND (MCC.MCC_CPG_NUM = 3299)
+        AND (MCC.MCC_CPG_NUM = 3583)
         AND (MCC.MCC_TIPO = 'P')
         AND (((MCC.MCC_IND_CPG_GERACAO IS NULL OR (MCC.MCC_IND_CPG_GERACAO = 'N'))
         AND (MCC.MCC_CRE = 0)) OR (MCC.MCC_IND_CPG_GERACAO = 'S')))
@@ -72,9 +72,21 @@ WHERE  ((MCC.MCC_CPG_SERIE = 119)
 --Alteração do Histórico da CFO (compromisso)
 
 UPDATE mcc
-SET mcc_obs = 'ITACLEAN COMERCIO DE PRODUTOS DE LIMPEZA'
-WHERE  ((MCC.MCC_CPG_SERIE = 1119)
-        AND (MCC.MCC_CPG_NUM = 3761)
+SET mcc_obs = 'ALCON'
+WHERE  ((MCC.MCC_CPG_SERIE = 119)
+        AND (MCC.MCC_CPG_NUM = 3394)
+        AND (MCC.MCC_TIPO = 'P')
+        AND (((MCC.MCC_IND_CPG_GERACAO IS NULL OR (MCC.MCC_IND_CPG_GERACAO = 'N'))
+        AND (MCC.MCC_CRE = 0)) OR (MCC.MCC_IND_CPG_GERACAO = 'S')))
+
+
+
+-------------------------------------------------------------------------
+--Alteração da CFO (compromisso)
+UPDATE mcc
+SET mcc_cfo_cod = 150
+WHERE  ((MCC.MCC_CPG_SERIE = 119)
+        AND (MCC.MCC_CPG_NUM = 3583)
         AND (MCC.MCC_TIPO = 'P')
         AND (((MCC.MCC_IND_CPG_GERACAO IS NULL OR (MCC.MCC_IND_CPG_GERACAO = 'N'))
         AND (MCC.MCC_CRE = 0)) OR (MCC.MCC_IND_CPG_GERACAO = 'S')))
@@ -86,7 +98,7 @@ INSERT INTO mcc (mcc_ccr, mcc_lote, mcc_seq, mcc_doc, mcc_dt_proc, mcc_dt, mcc_m
                 mcc_obs, mcc_cpg_serie, mcc_cpg_num, mcc_ipg_parc, mcc_str_cod, mcc_usr_login, mcc_cfo_estorno, mcc_fne_cod, mcc_cnv,
                 mcc_nominal_a, mcc_ind_cpg_geracao, mcc_dt_emissao, mcc_concilia, mcc_gcc_cod, mcc_seq_contrap, mcc_cct_cod, mcc_proced,
                 mcc_bcp_serie, mcc_bcp_num, mcc_qt_it, mcc_pti_cod, mcc_gr_ses_id, mcc_aee_id, mcc_seq_bcp_comp, mcc_cct_cpg_repasse, mcc_gcc_cod_ger)
-VALUES ('64051', 107124, 1, '1871','2019-11-26 09:54:49.570','2019-11-26 09:48:17.153','201911','P','1298.68',0,1119,'10237',
-        NULL,1119,3761,1,'999','MEDICWARE',NULL,NULL,NULL,
-        NULL,'S','2019-11-27 00:00:00.000','N',NULL,NULL,12260,'PAG-PTI',
-        NULL,NULL,NULL,NULL,1841034,NULL,NULL,NULL,NULL)
+VALUES ('88219', 107126, 1, '1873','2019-09-23 15:57:49.570','2019-09-23 15:58:17.153','201909','P','10233.35',0,119,'150',
+        'NOVARTIS BIOCIÊNCIAS S/A',119,3395,1,'999','MEDICWARE',NULL,NULL,NULL,
+        NULL,'S','2019-09-23 00:00:00.000','N',NULL,NULL,12240,'PAG-PTI',
+        NULL,NULL,NULL,NULL,1841036,NULL,NULL,NULL,NULL)
