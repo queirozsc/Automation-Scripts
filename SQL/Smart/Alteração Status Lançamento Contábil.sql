1ca043d11a3847c1b999187092de3e1dec6d469b
Willian Vilson da Cruz <willian.cruz@opty.com.br>
2020-01-08 16:54:11 -0300

    Update Contábil Hosl

--- SQL/Smart/Alteração Status Lançamento Contábil.sql
+++ SQL/Smart/Alteração Status Lançamento Contábil.sql
@@ -1,11 +1,11 @@
 --Consulta para selecionar os lançamento que será alterado o status, seja para cancelado ou ativo
 SELECT * 
 FROM LCT
-WHERE LCT_LOTE = 85328
-    AND LCT_SEQ IN (4,5)
+WHERE LCT_LOTE = 84004
+    AND LCT_SEQ IN (3)
 
 --Update para mudança de status do lançamento, seja para cancelado ou ativo
 UPDATE LCT
 SET LCT_DEL_LOGICA = 'S'
-WHERE LCT_LOTE = 85328
-    AND LCT_SEQ IN (4,5)
+WHERE LCT_LOTE = 84004
+    AND LCT_SEQ IN (3)
