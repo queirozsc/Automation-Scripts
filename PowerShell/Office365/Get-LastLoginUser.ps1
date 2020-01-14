fe3f55129c6ba393ea271ea16fefc076e15acd77
anderson.santos <gutosoethe@gmail.com>
2020-01-14 08:21:02 -0300

    Fixed Get-Date

--- PowerShell/Office365/Get-LastLoginUser.ps1
+++ PowerShell/Office365/Get-LastLoginUser.ps1
@@ -1,6 +1,9 @@
+.\Connect-Exchange.ps1
+.\Connect-O365.ps1
+
 $Users = Get-MsolUser -EnabledFilter All -MaxResults 5000 | Where-Object {$_.IsLicensed -eq $true}
 $licensetype = Get-MsolAccountSku | Where {$_.ConsumedUnits -ge 1}
-$data = Get-Date 2019-10-01
+$data = (Get-Date).AddDays(-90) 
 
 $ResultItem = @()
 
@@ -15,8 +18,8 @@ foreach ($User in $Users) {
                 $ResultItem += New-Object PSObject -Property @{ 
                     DisplayName = $UserLastLogon.DisplayName 
                     UserPrincipalName = $User.UserPrincipalName
-                    Marca =  $User.Office
                     LastLogon = $UserLastLogon.LastLogonTime.ToString("dd/MM/yyyy")
+                    Marca =  $User.Office
                 }
                 Write-Host $User.UserPrincipalName $UserLastLogon.LastLogonTime
         
