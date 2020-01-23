c86610b9f0335284ec9ba6e0bf4e42964db3fa0c
Willian Vilson da Cruz <willian.cruz@opty.com.br>
2020-01-23 17:58:19 -0300

    Updates

--- SQL/Smart/Alteração Status Lançamento Contábil.sql
+++ SQL/Smart/Alteração Status Lançamento Contábil.sql
@@ -1,35 +1,14 @@
 --Consulta para selecionar os lançamento que será alterado o status, seja para cancelado ou ativo
 SELECT * 
 FROM LCT
-WHERE LCT_LOTE = 102363
+WHERE LCT_LOTE = 28525
     AND LCT_SEQ IN (3)
 
 --Update para mudança de status do lançamento, seja para cancelado ou ativo
 UPDATE LCT
 SET LCT_DEL_LOGICA = 'S'
-WHERE LCT_LOTE = 102363
-    AND LCT_SEQ IN (6)
+WHERE LCT_LOTE = 28525
+    AND LCT_SEQ IN (3)
 
 --Consulta para descobrir o nnúmero do lote
-SELECT * FROM LCT WHERE lct_cpg_num = 17361 AND lct_cpg_serie = 119
-
-------------------------------------------------------------------------------------------
---Compromissos com falha HOSL
-
---Compromisso: 119.11524
-UPDATE LCT
-SET LCT_DEL_LOGICA = 'S'
-WHERE LCT_LOTE = 83232
-    AND LCT_SEQ IN (3)
-
---Compromisso: 119.11391
-UPDATE LCT
-SET LCT_DEL_LOGICA = 'S'
-WHERE LCT_LOTE = 83164
-    AND LCT_SEQ IN (3)
-
---Compromisso: 119.10884
-UPDATE LCT
-SET LCT_DEL_LOGICA = 'S'
-WHERE LCT_LOTE = 80976
-    AND LCT_SEQ IN (6)
+SELECT * FROM LCT WHERE lct_cpg_num = 7641 AND lct_cpg_serie = 119
