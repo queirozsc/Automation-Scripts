77e6b623139380a77b0d36ca21ac7bb6fef25d76
Willian Vilson da Cruz <willian.cruz@opty.com.br>
2020-02-06 12:32:38 -0300

    Updates

--- SQL/Smart/Alteração Status Lançamento Contábil.sql
+++ SQL/Smart/Alteração Status Lançamento Contábil.sql
@@ -1,14 +1,14 @@
 --Consulta para selecionar os lançamento que será alterado o status, seja para cancelado ou ativo
 SELECT * 
 FROM LCT
-WHERE LCT_LOTE = 28525
+WHERE LCT_LOTE = 114372
     AND LCT_SEQ IN (3)
 
 --Update para mudança de status do lançamento, seja para cancelado ou ativo
 UPDATE LCT
-SET LCT_DEL_LOGICA = 'S'
-WHERE LCT_LOTE = 28525
+SET LCT_DEL_LOGICA = 'N'
+WHERE LCT_LOTE = 84004
     AND LCT_SEQ IN (3)
 
 --Consulta para descobrir o nnúmero do lote
-SELECT * FROM LCT WHERE lct_cpg_num = 7641 AND lct_cpg_serie = 119
+SELECT * FROM LCT WHERE lct_cpg_num = 1175 AND lct_cpg_serie = 120
