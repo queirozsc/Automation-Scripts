98b1405421e1453ac8f909d1d3f926be2a1f25cf
anderson.santos <gutosoethe@gmail.com>
2020-02-21 16:34:22 -0300

    update

--- PowerShell/Computer/Create-LocalAdminUser.ps1
+++
@@ -1,3 +0,0 @@
-﻿New-LocalUser -Name CSI.Admin -Password  (ConvertTo-SecureString -String 'pwd@0pt1' -AsPlainText -Force) -PasswordNeverExpires 
-Get-LocalUser -Name 'CSI.Admin' | Set-LocalUser -FullName 'Administrador Local CSI'
-Get-LocalUser -Name 'CSI.Admin' | Add-LocalGroupMember -Group 'Administradores'
