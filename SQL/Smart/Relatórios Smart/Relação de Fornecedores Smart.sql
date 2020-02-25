0980bb6efc5e7c40c8b128ce917d012a4868e6d6
Willian Vilson da Cruz <willian.cruz@opty.com.br>
2020-02-25 16:50:13 -0300

    Versão Final

--- SQL/Smart/Relatórios Smart/Relação de Fornecedores Smart.sql
+++ SQL/Smart/Relatórios Smart/Relação de Fornecedores Smart.sql
@@ -1,45 +1,87 @@
-SELECT  emp.emp_cgc as 'CPF_CNPJ'
-        , emp.emp_nome_fantasia as 'NOME_PESSOA'
-        , CASE WHEN (emp.emp_fis_jur = 'J' OR (LEN(emp.emp_cgc) = 14)) THEN 'Jurídica'
-               WHEN (emp.emp_fis_jur = 'F' OR (LEN(emp.emp_cgc) = 11)) THEN 'Física'
-          END AS 'TIPO_PESSOA'
+--Relação de Fornecedores
+
+SELECT  cgc AS 'CPF_CNPJ'
+        , (SELECT TOP 1 emp_nome_fantasia FROM emp WHERE emp_cgc = cgc) AS 'NOME_PESSOA'
+        , (SELECT TOP 1 CASE WHEN (emp.emp_fis_jur = 'J' OR (LEN(emp.emp_cgc) = 14)) THEN 'Jurídica'
+                             WHEN (emp.emp_fis_jur = 'F' OR (LEN(emp.emp_cgc) = 11)) THEN 'Física'
+                        END AS 'TIPO_PESSOA'
+           FROM     EMP EMP
+           WHERE    emp_cgc = cgc
+          ) AS 'TIPO_PESSOA'
+        , (SELECT TOP 1 CASE WHEN fne.FNE_TIPO = 'FO' THEN 'Fornecedor'
+                             WHEN fne.FNE_TIPO = 'FT' THEN 'Forn_Transportador'
+                             WHEN fne.FNE_TIPO = 'TR' THEN 'Transportador'
+                             WHEN fne.FNE_TIPO = 'FF' THEN 'Forn_Fabricante'  
+                        END AS 'TIPO'
+           FROM     FNE FNE
+          ) AS 'TIPO'
+        , (SELECT TOP 1 CASE WHEN fne.FNE_STATUS = 'A' THEN 'Ativo'
+                             WHEN fne.FNE_STATUS = 'I' THEN 'Inativo'
+                        END AS 'STATUS'
+           FROM FNE FNE 
+          ) AS 'STATUS'
         , 'Itaigara' as EMPRESA
-FROM emp emp
-WHERE emp.emp_status NOT IN ('I','S')
-    AND emp.emp_cgc IN (SELECT emp.emp_cgc FROM emp emp GROUP BY emp.emp_cgc HAVING COUNT(1) > 1)
-ORDER BY emp.emp_cgc
-
-
-
-SELECT  emp.emp_cgc as 'CNPJ_CPF'
-        , fne.FNE_NOME_FANTASIA as 'NOME_PESSOA'
-        , CASE WHEN (emp.emp_fis_jur = 'J' OR (LEN(emp.emp_cgc) = 14)) THEN 'Jurídica'
-               WHEN (emp.emp_fis_jur = 'F' OR (LEN(emp.emp_cgc) = 11)) THEN 'Física'
-          END AS 'TIPO_PESSOA'
-        , CASE WHEN fne.FNE_TIPO = 'FO' THEN 'Fornecedor'
-               WHEN fne.FNE_TIPO = 'FT' THEN 'Forn_Transportador'
-               WHEN fne.FNE_TIPO = 'TR' THEN 'Transportador'
-               WHEN fne.FNE_TIPO = 'FF' THEN 'Forn_Fabricante'  
-          END AS 'TIPO'
-        , CASE WHEN fne.FNE_STATUS = 'A' THEN 'Ativo'
-               WHEN fne.FNE_STATUS = 'I' THEN 'Inativo'
-          END AS 'STATUS'
-        , 'Itaigara' as EMPRESA
-FROM    fne
-        LEFT JOIN emp
-            ON fne.FNE_COD = emp.emp_fne_cod
-WHERE   (fne.FNE_DEL_LOGICA IS NULL OR (fne.FNE_DEL_LOGICA = 'N'))
-        AND emp.emp_cgc IN ((SELECT emp.emp_cgc FROM emp emp GROUP BY emp.emp_cgc HAVING COUNT(1) > 1) OR 
-        AND fne.FNE_NOME_FANTASIA in (SELECT fne.FNE_NOME_FANTASIA FROM FNE fne GROUP BY fne.FNE_NOME_FANTASIA HAVING COUNT(1) > 1)
-ORDER BY 1 ASC
-
-
-
-SELECT fne.FNE_NOME_FANTASIA, COUNT(1) as quantidade 
-FROM   FNE fne
-GROUP BY FNE_NOME_FANTASIA HAVING COUNT(1) > 1
-OR
-SELECT emp.emp_cgc
-FROM emp emp
-GROUP BY emp.emp_cgc HAVING COUNT(1) > 1
-
+FROM    ((SELECT  (SELECT top 1 emp_cgc FROM emp WHERE emp_fne_cod = fne_cod) AS CGC        
+          FROM    fne        
+          WHERE   (fne.FNE_DEL_LOGICA IS NULL OR (fne.FNE_DEL_LOGICA = 'N'))
+                  AND fne.FNE_STATUS = 'A'
+        )) t1
+WHERE   cgc IS NOT NULL
+GROUP BY cgc HAVING count(cgc) > 1
+-----------------------------------------------------------------------------------------------------------------------------
+--Versão de Cadu
+/*
+SELECT cgc
+    , count(cgc)
+    , (
+        SELECT TOP 1 emp_nome_fantasia
+        FROM emp
+        WHERE emp_cgc = cgc
+        ) AS emp_nome
+FROM (
+    SELECT --emp.emp_cgc AS 'CNPJ_CPF'
+        fne.FNE_NOME_FANTASIA AS 'NOME_PESSOA'
+        -- , CASE 
+        --     WHEN (
+        --             emp.emp_fis_jur = 'J'
+        --             OR (LEN(emp.emp_cgc) = 14)
+        --             )
+        --         THEN 'Jurídica'
+        --     WHEN (
+        --             emp.emp_fis_jur = 'F'
+        --             OR (LEN(emp.emp_cgc) = 11)
+        --             )
+        --         THEN 'Física'
+        --     END AS 'TIPO_PESSOA'
+        , CASE 
+            WHEN fne.FNE_TIPO = 'FO'
+                THEN 'Fornecedor'
+            WHEN fne.FNE_TIPO = 'FT'
+                THEN 'Forn_Transportador'
+            WHEN fne.FNE_TIPO = 'TR'
+                THEN 'Transportador'
+            WHEN fne.FNE_TIPO = 'FF'
+                THEN 'Forn_Fabricante'
+            END AS 'TIPO'
+        , CASE 
+            WHEN fne.FNE_STATUS = 'A'
+                THEN 'Ativo'
+            WHEN fne.FNE_STATUS = 'I'
+                THEN 'Inativo'
+            END AS 'STATUS'
+        , 'Itaigara' AS EMPRESA
+        , (
+            SELECT top 1 emp_cgc
+            FROM emp
+            WHERE emp_fne_cod = fne_cod
+            ) AS CGC
+    FROM fne
+    WHERE (
+            fne.FNE_DEL_LOGICA IS NULL
+            OR (fne.FNE_DEL_LOGICA = 'N')
+            )
+        AND fne.FNE_STATUS = 'A'
+    ) t1
+WHERE cgc IS NOT NULL
+GROUP BY cgc
+HAVING count(cgc) > 1*/
