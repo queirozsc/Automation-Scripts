eacdd96562f34a201251f851c51e34f9355ccc57
anderson.santos <gutosoethe@gmail.com>
2020-03-02 13:45:00 -0300

    Update: Created Function

--- PowerShell/Office365/Export-O365GroupMember.ps1
+++ PowerShell/Office365/Export-O365GroupMember.ps1
@@ -1,65 +1,34 @@
 <#
 .SYNOPSIS
-  Example script to export Office 365 Groups with Members and Owners to CSV
+  
 .DESCRIPTION
-  This is an example script from Practical365.com to demonstrate exporting Office 365 Group / Team members and owners to CSV files
-.PARAMETER $CSVFilename
-  The path the the CSV file
+   
 .INPUTS
   None
 .OUTPUTS
   CSV File
 .NOTES
   Version:        1.0
-  Author:         Steve Goodman
-  Creation Date:  20190113
+  Author:         Anderson Soethe
+  Creation Date:  20200302
   Purpose/Change: Initial version
   
 .EXAMPLE
-  Export-O365Groups -CSVFilename .\Groups.CSV
+  Export-O365DistrMembers $FileName $DistrName 
 #>
-param([parameter(Mandatory=$True, HelpMessage='Please enter a filename for the CSV file to export')]$CSVFilename)
 
-Write-Host -ForegroundColor Green "Loading all Office 365 Groups"
-$Groups = Get-UnifiedGroup -ResultSize Unlimited
-
-# Process Groups
-$GroupsCSV = @()
-Write-Host -ForegroundColor Green "Processing Groups"
-foreach ($Group in $Groups)
-{
-    
-    # Get  members
-    Write-Host -ForegroundColor Yellow -NoNewline "."
-    $Members = Get-UnifiedGroupLinks -Identity $Group.Identity -LinkType Members -ResultSize Unlimited
-    $MembersSMTP=@()
-    foreach ($Member in $Members)
-    {
-        $MembersSMTP+=$Member.PrimarySmtpAddress        
-    }
-    # Get  owners
-    $Owners = Get-UnifiedGroupLinks -Identity $Group.Identity -LinkType Owners -ResultSize Unlimited
-    $OwnersSMTP=@()
-    foreach ($Owner in $Owners)
-    {
-            
-        $OwnersSMTP+=$Owner.PrimarySmtpAddress
-    }
-    
-    # Create CSV file line
-    $GroupsRow =   [pscustomobject]@{
-                    GroupSMTPAddress = $Group.PrimarySmtpAddress
-                    GroupIdentity = $Group.Identity
-                    GroupDisplayName = $Group.DisplayName
-                    MembersSMTP = $MembersSMTP -join "`n"
-                    OwnersSMTP = $OwnersSMTP -join "`n"
-                    }
-
-    # Add to export array
-    $GroupsCSV+=$GroupsRow
-        
+function Export-O365DistrMembers {
+  param (
+    $FileName,$DistrName
+  )
+    Write-Host "Processing Data..."
+    $Members = Get-DistributionGroupMember -Identity $DistrName -ErrorAction Stop | Select-Object DisplayName, PrimarySmtpAddress
+    Write-Host "Builded and Exported with sucess"
+    $Members | Export-Csv -Path $Filename -Encoding UTF8 -Delimiter ";" -NoTypeInformation
 }
 
-# Export to CSV
-Write-Host -ForegroundColor Green "`nCreating and exporting CSV file"
-$GroupsCSV | Export-Csv -NoTypeInformation -Path $CSVFilename
+$FileName = Read-Host "Informe um para para o arquivo CSV"
+$DistrName = Read-Host "Informe o nome da Lista de Distribuição"
+
+Export-O365DistrMembers $FileName $DistrName 
+
